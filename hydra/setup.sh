908b3015ef718e435bb2ae3066014619165f027f
Daniel Iser <daniel@code-atlantic.com>
2023-02-15 05:50:31 -0500

    Update with better ui & options for granting ro/rw

--- hydra/setup.sh
+++ hydra/setup.sh
@@ -1,7 +1,33 @@
 #!/bin/bash
 
+source ../bin/inquiry.sh
+
 # Define the log file path
 LOG_FILE="commands.log"
+
+note=""
+
+# Function to clear the screen and display the title
+refresh_screen() {
+    clear
+    echo Open Source Data Pipeline Stack Assistant
+    len=$((${#note} + 6))
+    # check if note is empty, if so set len to 40
+    if [[ -z "$note" ]]; then
+        len=40
+    fi
+
+    printf '%0.s-' $(seq 1 $len)
+    echo
+    # if $note is not empty, display it
+    if [[ -n "$note" ]]; then
+        echo "-- $note --"
+        # echo - for every character in $note + 6, minimum 40
+        printf '%0.s-' $(seq 1 $len)
+        echo
+    fi
+    echo ""
+}
 
 # Function to log a command and then execute it
 log_and_execute() {
@@ -9,7 +35,10 @@ log_and_execute() {
     echo "Running command: $command"
     echo "$command" >>"$LOG_FILE"
     eval "$command"
+    refresh_screen
 }
+
+refresh_screen
 
 # Start the hydra container.
 log_and_execute "docker-compose -f ./docker-compose.yml up -d"
@@ -32,6 +61,10 @@ while true; do
     # Wait 1 second before checking again.
     sleep 1
 done
+
+note="Set up superuser for Postgres database."
+
+refresh_screen
 
 # Prompt for new superuser name & password
 read -p "Enter a new superuser name: " superuser_name
@@ -83,6 +116,10 @@ else
     echo "Created user $superuser_name with password *********"
 fi
 
+note="Set up new database"
+
+refresh_screen
+
 # From here on out we will use the new superuser to create databases and users
 echo "Using user $superuser_name to create databases, schemas, & users"
 db_user=$superuser_name
@@ -92,32 +129,108 @@ while true; do
     read -p "Do you need a new database? (y/n) " -n 1 -r
     echo
     if [[ $REPLY =~ ^[Yy]$ ]]; then
+        note="Set up new database"
+        refresh_screen
+
         read -p "Enter a new database name: " database_name
         log_and_execute "docker exec hydra sh -c \"psql -U $db_user -d postgres -c \\\"CREATE DATABASE $database_name;\\\"\""
         echo "Created database $database_name"
+
+        note="Set up schemas for database $database_name"
+        refresh_screen
+
+        # Prompt if they need to create a new schema, if so prompt for name and create it
+        while true; do
+            read -p "Do you need a new schema? (y/n) " -n 1 -r
+            echo
+            if [[ $REPLY =~ ^[Yy]$ ]]; then
+                read -p "Enter a new schema name: " schema_name
+                log_and_execute "docker exec hydra sh -c \"psql -U $db_user -d $database_name -c \\\"CREATE SCHEMA $schema_name;\\\"\""
+                echo "Created schema $schema_name"
+            elif [[ $REPLY =~ ^[Nn]$ ]]; then
+                break
+            else
+                echo "Please enter y or n."
+            fi
+        done
     elif [[ $REPLY =~ ^[Nn]$ ]]; then
         break
     else
         echo "Please enter y or n."
     fi
 done
+
+# Get list of databases
+databases=($(docker exec hydra sh -c "psql -U postgres -d postgres -t -c 'SELECT datname FROM pg_database;'"))
+
+# Get list of schemas
+schemas=($(docker exec hydra sh -c "psql -U $db_user -d postgres -t -c 'SELECT schema_name FROM information_schema.schemata;'"))
+
+note="Set up users"
+refresh_screen
 
 # Prompt if they need additional users, if so give continous prompt until they hit escape, then create them
 while true; do
     read -p "Do you need to create a new user? (y/n) " -n 1 -r
     echo
     if [[ $REPLY =~ ^[Yy]$ ]]; then
+
         read -p "Enter a new user name: " user_name
         get_password
         log_and_execute "docker exec hydra sh -c \"psql -U $db_user -d postgres -c \\\"CREATE USER $user_name WITH PASSWORD '$password';\\\"\""
         echo "Created user $user_name with password *********"
-    elif [[ $REPLY =~ ^[Nn]$ ]]; then
+
+        # Prompt if they need to grant privileges to the new user, if so render checkbox_list of databases and schemas
+        while true; do
+            read -p "Do you need to grant privileges to the new user? (y/n) " -n 1 -r
+            echo
+            if [[ $REPLY =~ ^[Yy]$ ]]; then
+
+                # Render checkbox list of databases and schemas
+                echo "Select the databases you want to grant privileges to the new user for."
+                echo "Press space to select and enter to continue."
+                echo "Press escape to skip this step."
+                # Use checkbox_list to prompt user for selection
+                checkbox_input "Select Databases for Read Only:" databases ro_database_selections
+
+                # Use checkbox_list to prompt user for selection
+                checkbox_input "Select Databases for Full Access:" databases rw_database_selections
+
+                # Foreach ro_database_selections grant read only privileges to the new user
+                for database in "${ro_database_selections[@]}"; do
+                    log_and_execute "docker exec hydra sh -c \"psql -U $db_user -d postgres -c \\\"GRANT CONNECT ON DATABASE $database TO $user_name;\\\"\""
+                    log_and_execute "docker exec hydra sh -c \"psql -U $db_user -d postgres -c \\\"GRANT USAGE ON SCHEMA public TO $user_name;\\\"\""
+                    log_and_execute "docker exec hydra sh -c \"psql -U $db_user -d postgres -c \\\"GRANT SELECT ON ALL TABLES IN SCHEMA public TO $user_name;\\\"\""
+                    log_and_execute "docker exec hydra sh -c \"psql -U $db_user -d postgres -c \\\"GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO $user_name;\\\"\""
+                    log_and_execute "docker exec hydra sh -c \"psql -U $db_user -d postgres -c \\\"GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA public TO $user_name;\\\"\""
+                    echo "Granted read only privileges to user $user_name on database $database"
+                done
+
+                # Foreach rw_database_selections grant full access privileges to the new user
+                for database in "${rw_database_selections[@]}"; do
+                    log_and_execute "docker exec hydra sh -c \"psql -U $db_user -d postgres -c \\\"GRANT CONNECT ON DATABASE $database TO $user_name;\\\"\""
+                    log_and_execute "docker exec hydra sh -c \"psql -U $db_user -d postgres -c \\\"GRANT ALL PRIVILEGES ON DATABASE $database TO $user_name;\\\"\""
+                    echo "Granted full access privileges to user $user_name on database $database"
+                done
+
+                break
+            elif [[ $REPLY =~ ^[Nn]$ ]]; then
+                break
+            else
+                echo "Please enter y or n."
+            fi
+        done
+
+    elif
+        [[ $REPLY =~ ^[Nn]$ ]]
+    then
         break
     else
         echo "Please enter y or n."
     fi
 done
 
+note="Hydra is ready to go!"
+refresh_screen
 
-# Get list of databases
-databases=($(docker exec hydra sh -c "psql -U postgres -d postgres -t -c 'SELECT datname FROM pg_database;'"))
+exit
